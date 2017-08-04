5857c8949767b043797852feb37ebaa9a5204d96
Thaeng <30720716+Thaeng@users.noreply.github.com>
2017-08-04 15:16:02 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,116 @@
+var player, floorHeight;
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  player = new unitPlayer(50,50);
+}
+
+function draw() {
+  frameRate(2);
+  background(90);
+  drawFloor();
+  player.drawBody();
+}
+
+function unitPlayer(sizeX, sizeY){
+  this.sizeX = sizeX;
+  this.sizeY = sizeY;
+  this.offsetY = 0;
+  this.offsetX = 0;
+  this.orientation = radians(0);
+  this.isJumping = false;
+  var x = 0;
+  var y = 0;
+  var reachedTop = false;
+  var jumpHeight = 400;
+  var finalOffsetY = 0;
+  var countAfterImages = 5;
+  var afterImages = [countAfterImages];
+  var afterImagesOrientation = [countAfterImages];
+  var counter = 0;
+  var finalCounter = 0;
+  var heading = "right";
+
+  this.drawBody = function(){
+    if("right" == heading){
+      this.drawAfterImage(x,y,this.orientation,this.sizeX,this.sizeY);
+    }else if("left" == heading){
+      this.drawAfterImage(x,y,-1*this.orientation,this.sizeX,this.sizeY);
+    }
+    push();
+      this.checkBehavior();
+      rectMode(CENTER);
+      x = width*0.15 - this.offsetX;
+      y = (floorHeight-this.sizeY/2)-finalOffsetY;
+      translate(x,y);
+      if("right" == heading){
+        rotate(this.orientation);
+      }else if("left" == heading){
+        rotate(-1*this.orientation);
+      }
+      fill(color(100,200,100));
+      rect(0,0,this.sizeX,this.sizeY,40,40,0,0);
+    pop();
+  }
+
+  this.checkBehavior = function(){
+    if(this.isJumping == true){
+      this.jump();
+    }
+    if(keyIsDown(LEFT_ARROW) || keyIsDown(65)){
+      this.offsetX+=6;
+      heading = "left";
+    }else if(keyIsDown(RIGHT_ARROW) || keyIsDown(68)){
+      this.offsetX-=6;
+      heading = "right";
+    }
+    if(keyIsDown(UP_ARROW) || keyIsDown(32) || keyIsDown(87)){
+      player.isJumping = true;
+    }
+  }
+
+  this.jump = function(){
+    this.orientation += radians(360/40);
+    this.offsetY++;
+    finalOffsetY = 0.4*(-1*((this.offsetY-sqrt(jumpHeight))*(this.offsetY-sqrt(jumpHeight))) + jumpHeight);
+    if(finalOffsetY < 0){
+      this.offsetY = 0;
+      this.isJumping =  false;
+      finalOffsetY = 0;
+      this.orientation = radians(0);
+    }
+  }
+
+  this.drawAfterImage = function(x,y,orientation,sizeX,sizeY){
+    afterImages[counter] = createVector(x,y);
+    afterImagesOrientation[counter] = orientation;
+
+    for(var i = 0; i < finalCounter; i++){
+      push();
+      rectMode(CENTER);
+      noStroke();
+      translate(afterImages[i].x,afterImages[i].y);
+      rotate(afterImagesOrientation[i]);
+      fill(color(140,220,0,20));
+      rect(0,0,sizeX,sizeY,40,40,0,0);
+      pop();
+    }
+    counter++;
+    finalCounter++;
+    if(counter >= countAfterImages){
+      counter = 0;
+    }
+    if(finalCounter > countAfterImages){
+      finalCounter = countAfterImages;
+    }
+  }
+
+}
+
+function drawFloor(){
+  floorHeight =  height * 0.7;
+  push();
+    fill(200);
+    rect(0,floorHeight,width,height-floorHeight
+    );
+  pop();
+}
